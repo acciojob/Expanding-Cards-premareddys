183252503e0e51802171e5b333c4bd1a17019aba
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-12-13 21:54:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function expandPanel(panelNumber) {
+    // Remove 'active' class from all panels
+    document.querySelectorAll('.panel').forEach(panel => {
+      panel.classList.remove('active');
+    });
+
+    // Add 'active' class to the clicked panel
+    document.getElementById(panel-${panelNumber}).classList.add('active');
+  }
